a902ee3540cb574f781a5342b9f6826b6cce930c
MISFOfficial <muksitul44@gmail.com>
2026-05-30 08:46:35 +0600

    chore: add r2.dev domain to next.config.ts image allowlist

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["i.ibb.co.com", "muksitul.biddaneer.com"],
+    domains: [
+      "i.ibb.co.com",
+      "muksitul.biddaneer.com",
+      "pub-bb13b0ae287e4dcaa9ef3ce08a211ce8.r2.dev",
+    ],
   },
   experimental: {
     optimizePackageImports: ["lucide-react", "gsap", "swiper"],
